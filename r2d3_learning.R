1e4b0c9be298bf4fb472d3a451925bb0a203ae18
L. D. Nicolas May <LDNicolasMay@gmail.com>
2018-12-23 10:35:37 -0500

    add d3 barchart, shiny app demo

--- r2d3_learning.R
+++ r2d3_learning.R
@@ -1,2 +1,10 @@
 # r2d3_learning.R
 
+library(r2d3)
+
+data <- c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
+r2d3(data=data, script = "barchart.js")
+r2d3(data=rev(data), script = "barchart.js")
+r2d3(data=sample(data, size=length(data), replace=FALSE), 
+     script = 'barchart.js')
+
